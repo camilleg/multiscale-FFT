6462c38cc208cfd5d9d01f45c7f7a1bb25b515a7
camilleg <camilleg@camille-g.com>
2022-06-06 15:08:37 -0500

    port to macOS

--- check.sh
+++ check.sh
@@ -3,8 +3,24 @@
 # Does the output of ./stft still match the 2009 output,
 # which matched MATLAB's output?
 
-a=$(more stft_test_*.csv | md5sum)
-if [[ "$a" != "edf43a9fc57bba7beb850733fb58b9d1  -" ]]; then
+uname=$(uname -s 2>/dev/null)
+if [[ "$uname" == "Darwin" ]]; then
+  # On macOS, change all nan to -nan, to match Linux.
+  if ! grep -q -e '-nan' stft_test_sinc.csv; then
+    # Found no '-nan', thus we won't be changing '-nan' to '--nan'.
+    sed -I '' 's/nan,/-nan,/g' stft_test_sinc.csv
+  fi
+# elif [[ "$uname" == "Linux" ]]; then
+#   :
+fi
+
+# Use shasum instead of more familiar tools:
+# - macOS's md5 differs in output format from linux's md5sum.
+# - macOS's `more` differs, even with LESS_IS_MORE=1.
+# - .tar files differ in file owner and timestamp.
+
+a=$(shasum stft_test_*.csv | shasum - | sed 's/ .*//')
+if [[ "$a" != "bdf6a1f46ab79888d1d43bb07eef23a99df21bbd" ]]; then
   echo "stft_test_*.csv is incorrect."
   exit 1
 fi
